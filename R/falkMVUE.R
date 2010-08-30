f24a47f1621c1d2ef24782d1dbcbc5eccaf4ee6c
Kaspar Rufibach <kaspar.rufibach@ifspm.uzh.ch>
2010-08-30 00:00:00 +0000

    version 2.0.0

--- R/falkMVUE.R
+++ R/falkMVUE.R
@@ -1,26 +1,44 @@
-`falkMVUE` <-
-function (x, omega) 
-{
-    n <- length(x)
-    v1 <- 1:n * NA
-    v2 <- v1
-    if (omega < x[n]) {
-        cat("omega must be greater than max(x)!")
+falkMVUE <- function(est, omega, ks = NA){
+#
+# Calculate Falk's minimum variance unbiased tail index estimator, for a known endpoint.
+#
+# Input:
+# - x   : Vector of quantiles. If the ordinary Pickand estimator is to be calculated,
+#         x simply equals the vector of ordered observations.
+# - gam : Known tail index.
+#
+# Kaspar Rufibach, 2010
+#
+n <- est$n
+x <- est$xn 
+v1 <- 1:n*NA
+v2 <- v1
+
+if (omega < x[n]){
+    cat("omega must be greater than max(x)!")} else {
+        
+    # calculate quantiles 
+    c <- 1:n
+    q <- quantilesLogConDens(ps = c / n, est)[, "quantile"]
+
+    k0 <- 2:(n - 1)
+    if (identical(NA, ks)){ks <- k0}
+    ks <- ks[(ks %in% k0)]
+
+    for (k in ks){
+        j <- 2:k
+    
+        # Falks based on quantiles of log-concave 
+        temp <- (omega - q[n-j+1]) / (omega - q[n-k])
+        v1[k] <- sum(log(temp)) / k
+    
+        # Falks based on order statistics 
+        temp <- (omega - x[n-j+1]) / (omega - x[n-k])
+        v2[k] <- sum(log(temp)) / k
     }
-    else {
-        est <- activeSetLogCon(x, print = FALSE)
-        q <- 1:n * NA
-        for (c in 1:n) {
-            q[c] <- quantilesLogConDens(c/n, x, est$phi, est$Fhat)
-        }
-        for (k in 2:(n - 1)) {
-            j <- 2:k
-            temp <- (omega - q[n - j + 1])/(omega - q[n - k])
-            v1[k] <- sum(log(temp))/k
-            temp <- (omega - x[n - j + 1])/(omega - x[n - k])
-            v2[k] <- sum(log(temp))/k
-        }
-        res <- cbind(k = 1:n, logcon = v1, order = v2)
-        return(res)
-    }
+
+res <- cbind("k" = 1:n, "logcon" = v1, "order" = v2) 
+
+return(res)}
 }
+
